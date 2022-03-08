e1f43e08e62191000b566776384e3758d3ccd5cb
Tharles de Sousa Andrade <irtharles@gmail.com>
2022-03-08 11:40:37 -0300

    config CORS 06

--- src/server/app.js
+++ src/server/app.js
@@ -24,14 +24,7 @@ app.database.client.init(function () {
     app.libs.catalog.init(function () {
         app.middleware.repository.init(() => {
             app.use(cookie);
-            app.use((req, res, next) => {
-                res.setHeader("Access-Control-Allow-Origin", "*");
-                res.setHeader("Access-Control-Allow-Credentials", "true");
-                res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-                res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-                next();
-            });
-
+            app.use(cors())
             app.use(compression());
             app.use(express.static(app.config.clientDir));
             app.set('views', __dirname + '/templates');
