6736563984157209893d072bf16f43a8046543a9
Tharles de Sousa Andrade <irtharles@gmail.com>
2022-03-08 10:51:51 -0300

    config CORS 05

--- src/server/app.js
+++ src/server/app.js
@@ -25,8 +25,10 @@ app.database.client.init(function () {
         app.middleware.repository.init(() => {
             app.use(cookie);
             app.use((req, res, next) => {
-                res.setHeader('Access-Control-Allow-Origin', '*');
-                res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+                res.setHeader("Access-Control-Allow-Origin", "*");
+                res.setHeader("Access-Control-Allow-Credentials", "true");
+                res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+                res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
                 next();
             });
 
