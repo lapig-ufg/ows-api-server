9a4a3df4b1e6d403c33f287867280086762103ec
Jairo Matos da Rocha <devjairomr@gmail.com>
2023-12-08 12:47:02 -0300

    correção de nomes

--- src/server/controllers/ogc-server.js
+++ src/server/controllers/ogc-server.js
@@ -283,8 +283,8 @@ module.exports = function (app) {
 
 		if(
 			regexGeral.test(params['MODE']) === false ||
-			regexGeral.test(params['TITLE']) === false ||
-			regexGeral.test(params['TITLEMODE']) === false ||
+			regexGeral.test(params['TILE']) === false ||
+			regexGeral.test(params['TILEMODE']) === false ||
 			regexGeral.test(params['MAP.IMAGETYPE']) === false
 			) {
 			return response.json({
